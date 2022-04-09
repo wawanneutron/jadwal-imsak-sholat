58cc4c8c04eb0dd9ed9c9bb7127c7538d4061ed4
wawanneutron <wawanneutron1331@gmail.com>
2022-04-09 10:30:28 +0700

    footer

---
+++ src/components/FooterBottom.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2022 Copyright:
+      <a
+        class="text-dark"
+        target="__blank"
+        href="https://github.com/wawanneutron"
+        >wawanneutron</a
+      >
+    </div>
+    <!-- Copyright -->
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  margin-top: 180px;
+}
+</style>
