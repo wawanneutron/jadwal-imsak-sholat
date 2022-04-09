b03261d489487a39e73a3756f14744d472141048
wawanneutron <wawanneutron1331@gmail.com>
2022-04-09 10:32:03 +0700

    first card

---
+++ src/components/CardJadwal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row mt-5">
+      <template v-for="(item, index) in dataApi" :key="index">
+        <div class="col-md-3">
+          <div class="card mb-2">
+              <p>
+                  <ul>
+                    <li>{{ item.date }}</li>
+                    <li>{{ item.imsak }}</li>
+                  </ul>
+              </p>
+            </div>
+        </div>
+      </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CardJadwal",
+    props: {
+      dataApi: Array,
+    },
+  };
+</script>
