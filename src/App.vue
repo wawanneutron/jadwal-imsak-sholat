ccf42dc6282230f024f04f49f1299dc35be88c0b
wawanneutron <wawanneutron1331@gmail.com>
2022-04-09 10:31:42 +0700

    import component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <CarouselHeader />
+    <JadwalImsyakSholat />
+  </div>
+  <Footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JadwalImsyakSholat from "@/components/FormRegionJadwal.vue";
+import CarouselHeader from "@/components/CarouselHeader.vue";
+import Footer from "@/components/FooterBottom.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    JadwalImsyakSholat,
+    CarouselHeader,
+    Footer,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
