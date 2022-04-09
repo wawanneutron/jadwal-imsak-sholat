1f2d16041998940b68ea6bc1f9770c9c67b345c6
wawanneutron <wawanneutron1331@gmail.com>
2022-04-09 10:30:56 +0700

    form region

---
+++ src/components/FormRegionJadwal.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="row justify-content-center jadwal-imsak">
+    <div class="title mb-2 text-center">
+      <h2>Jadwal Imsakiyah</h2>
+      <h4>Data ini diambil dari kementrian agama republik Indonesia</h4>
+    </div>
+    <div class="col-md-7">
+      <section class="choice-region">
+        <div class="card">
+          <div class="card-header bg-success">
+            <span>Jadwal Sholat</span>
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <label for="exampleFormControlInput1" class="form-label"
+                    >Provinsi</label
+                  >
+                  <select
+                    class="form-select form-select-lg mb-3"
+                    aria-label="form-select-lg example"
+                    id="state"
+                    v-model="state.state_id"
+                    @change="getCities"
+                  >
+                    <option selected>-- Pilih Provinsi --</option>
+                    <option
+                      v-for="(item, index) in stateApi"
+                      :key="index"
+                      :value="item.id"
+                    >
+                      {{ item.name }}
+                    </option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="mb-3">
+                  <label for="exampleFormControlInput1" class="form-label"
+                    >Kabupaten/Kota</label
+                  >
+                  <select
+                    class="form-select form-select-lg mb-3"
+                    aria-label="form-select-lg example"
+                    v-model="state.city_id"
+                    @change="jadwalImsak"
+                  >
+                    <option selected>-- Pilih Kota --</option>
+                    <option v-for="(item, index) in state.cities" :key="index">
+                      {{ item.name }}
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div class="col-md-3">
+      <section class="menu">
+        <div class="card">
+          <div class="card-header bg-success">
+            <span>Menu Lainnya</span>
+          </div>
+          <div class="card-body">
+            <a href="#" class="btn">Jadwal Sholat</a>
+            <a href="#" class="btn">Jadwal Imsak</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+  <Card :dataApi="state.dataImsak" />
+</template>
+
+<script>
+import Card from "@/components/CardJadwal.vue";
+import { computed, onMounted, reactive } from "@vue/runtime-core";
+import { useStore } from "vuex";
+import Api from "@/api/Api";
+
+export default {
+  components: {
+    Card,
+  },
+  setup() {
+    const state = reactive({
+      state_id: "",
+      city_id: "",
+      cities: [],
+      dataImsak: [],
+    });
+
+    const store = useStore();
+
+    onMounted(() => {
+      store.dispatch("stateApi/getStateApi");
+    });
+
+    const stateApi = computed(() => {
+      return store.getters["stateApi/getStateApi"].slice(1);
+    });
+
+    // dapatkan kota
+    const getCities = () => {
+      console.info(state.state_id);
+      Api.get("/city", {
+        params: {
+          state: state.state_id,
+        },
+      })
+        .then((response) => {
+          state.cities = response.data.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+
+    // dapatkan jadwal imsak
+    const jadwalImsak = () => {
+      Api.get("/imsyakiyah", {
+        params: {
+          state: state.state_id,
+          city: state.city_id,
+          year: "2022",
+        },
+      })
+        .then((response) => {
+          state.dataImsak = response.data.data;
+          console.info(state.dataImsak);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
+
+    return {
+      state,
+      stateApi,
+      getCities,
+      jadwalImsak,
+    };
+  },
+};
+</script>
