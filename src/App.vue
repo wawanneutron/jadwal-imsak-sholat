3c4a085d31c416e71caba0be19381506275331c0
wawanneutron <wawanneutron1331@gmail.com>
2022-04-10 08:06:06 +0700

    import component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
+  <HeaderBanner />
   <div class="container">
-    <CarouselHeader />
     <JadwalImsyakSholat />
   </div>
   <Footer />
+  <Scroll />
 </template>
 
 <script>
+import HeaderBanner from "@/components/CarouselBanner.vue";
 import JadwalImsyakSholat from "@/components/FormRegionJadwal.vue";
-import CarouselHeader from "@/components/CarouselHeader.vue";
 import Footer from "@/components/FooterBottom.vue";
+import Scroll from "@/components/ScrollTop.vue";
 
 export default {
   name: "App",
   components: {
+    HeaderBanner,
     JadwalImsyakSholat,
-    CarouselHeader,
     Footer,
+    Scroll,
   },
 };
 </script>
