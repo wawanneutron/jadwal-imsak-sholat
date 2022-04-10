b3d8d54e64965f567032a1a30cdcb8ecf7273617
wawanneutron <wawanneutron1331@gmail.com>
2022-04-10 08:41:05 +0700

    convert to scss

--- src/components/CardJadwal.vue
+++ src/components/CardJadwal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row mt-5 row-card-jadwal">
+  <div class="row mt-5 row-jadwalsholat">
     <div class="title mb-2">
       <template v-for="(item, index) of dataMeta" :key="index">
         <h4>
@@ -15,7 +15,7 @@
     </div>
     <template v-for="(property, index) of dataApi" :key="index">
       <div class="col-md-4">
-        <div class="card jadwal-sholat mb-2 mt-2">
+        <div class="card mb-2 mt-2">
           <div class="card-body">
             <div class="row">
               <div class="col-4 mb-2">
@@ -92,29 +92,27 @@ export default {
 };
 </script>
 
-<style>
-.row-card-jadwal {
+<style lang="scss">
+.row-jadwalsholat {
   margin-bottom: 110px;
-}
-i.icon-jadwal {
-  background-image: url("../assets/icon/waktu-sholat-imsak.svg");
-}
-.jadwal-sholat i.icon-jadwal {
-  display: inline-block;
-  background-size: 100%;
-  background-repeat: no-repeat;
-  margin: 10px;
-  width: 20px;
-  height: 20px;
-}
-.jadwal-sholat .waktu-sholat {
-  display: inline-block;
-}
-.jadwal-sholat .waktu-sholat .title,
-.jadwal-sholat .waktu-sholat .waktu {
-  display: block;
-  line-height: 18px;
-  font-size: 14px;
-  font-weight: bold;
+  .icon-jadwal {
+    background-image: url("../assets/icon/waktu-sholat-imsak.svg");
+    display: inline-block;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    margin: 10px;
+    width: 20px;
+    height: 20px;
+  }
+  .waktu-sholat {
+    display: inline-block;
+    .title,
+    .waktu {
+      display: block;
+      line-height: 18px;
+      font-size: 14px;
+      font-weight: bold;
+    }
+  }
 }
 </style>
