682def43d3b875fcc2be27ee27d1165ae688e992
wawanneutron <wawanneutron1331@gmail.com>
2022-04-10 11:50:14 +0700

    responsive icon footer

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -2,9 +2,9 @@
   <!-- Bottom Navbar -->
   <nav class="navbar navbar-dark bg-nav-bottom navbar-expand fixed-bottom">
     <ul class="navbar-nav justify-content-center w-100">
-      <li class="nav-item text-center me-5">
+      <li class="nav-item text-center">
         <a href="#" class="nav-link">
-          <div>
+          <div class="icon_footer">
             <img
               src="../assets/icon/waktu-sholat-imsak_1.svg"
               alt=""
@@ -16,8 +16,8 @@
       </li>
       <li class="nav-item text-center pointer">
         <a href="#" class="nav-link" @click="jadwalSholat">
-          <div>
-            <img src="../assets/icon/icon_sholat_1.svg" alt="" srcset="" />
+          <div class="icon_footer">
+            <img src="../assets/icon/ic_sholat.svg" alt="" srcset="" />
           </div>
           Jadwal Sholat
         </a>
@@ -36,9 +36,48 @@ export default {
 };
 </script>
 
-<style lang="css">
+<style lang="scss">
 .bg-nav-bottom {
-  /* background-color: #215d71; */
   background-color: #10495c;
+  padding: 0;
+  .icon_footer {
+    img {
+      width: 35px;
+    }
+  }
+  li {
+    margin: auto;
+  }
+}
+
+@media (min-width: 576px) {
+  .bg-nav-bottom {
+    background-color: #10495c;
+    .icon_footer {
+      img {
+        width: 35px;
+      }
+    }
+    li {
+      margin-left: 30px;
+      margin-right: 30px;
+    }
+  }
+}
+
+@media (min-width: 768px) {
+  .bg-nav-bottom {
+    /* background-color: #215d71; */
+    background-color: #10495c;
+    .icon_footer {
+      img {
+        width: 50px;
+      }
+    }
+    li {
+      margin-left: 35px;
+      margin-right: 35px;
+    }
+  }
 }
 </style>
