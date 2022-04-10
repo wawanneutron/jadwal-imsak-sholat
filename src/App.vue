92c421586d80c373cdf9e44b8f9f172f9631550a
wawanneutron <wawanneutron1331@gmail.com>
2022-04-10 11:49:42 +0700

    import font scss

--- src/App.vue
+++ src/App.vue
@@ -23,3 +23,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Rubik:300,400,500,600");
+* {
+  font-family: "Asap", sans-serif;
+}
+</style>
