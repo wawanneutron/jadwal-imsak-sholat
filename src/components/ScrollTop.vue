c8fa7ee788a23f370866fcb0ec60b1259ff47df3
wawanneutron <wawanneutron1331@gmail.com>
2022-04-12 21:17:57 +0700

    fix bottom scroll top

--- src/components/ScrollTop.vue
+++ src/components/ScrollTop.vue
@@ -1,24 +1,26 @@
 <template>
   <!-- <scroll-top- /> -->
-  <a
-    @click="scrollTop"
-    :class="{ 'hidden-button': visible }"
-    class="btn btn-arrow btn-light"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      class="bi bi-arrow-up-circle"
-      viewBox="0 0 16 16"
+  <div class="totop">
+    <a
+      @click="scrollTop"
+      :class="{ 'hidden-button': visible }"
+      class="btn btn-arrow btn-light"
     >
-      <path
-        fill-rule="evenodd"
-        d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"
-      />
-    </svg>
-  </a>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-arrow-up-circle"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"
+        />
+      </svg>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -55,7 +57,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.btn-arrow {
+.totop {
   svg {
     color: #10495c;
     border-radius: 50%;
